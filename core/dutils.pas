cf3887d8d384164202955e8a6707bde8618ef19c
silvioprog <silvioprog@gmail.com>
2014-03-03 12:03:38 -0300

    Added top functions.

--- core/dutils.pas
+++ core/dutils.pas
@@ -18,13 +18,17 @@ unit dUtils;
 interface
 
 uses
-  DB, SysUtils, TypInfo;
+  dClasses, DB, SysUtils, TypInfo;
 
 procedure dParameterizeSQL(var ASQL: string; AParams: TParams);
 procedure dGetFields(AObject: TObject; AFields: TFields);
+procedure dSetFields(APropList: PPropList; const APropCount: Integer;
+  AObject: TObject; AFields: TFields); overload;
+procedure dSetFields(AObject: TObject; AFields: TFields); overload;
 procedure dGetParams(AObject: TObject; AParams: TParams);
-procedure dSetFields(AObject: TObject; AFields: TFields);
-procedure dSetParams(AObject: TObject; AParams: TParams);
+procedure dSetParams(APropList: PPropList; const APropCount: Integer;
+  AObject: TObject; AParams: TParams); overload;
+procedure dSetParams(AObject: TObject; AParams: TParams); overload;
 
 implementation
 
@@ -72,6 +76,48 @@ begin
   end;
 end;
 
+procedure dSetFields(APropList: PPropList; const APropCount: Integer;
+  AObject: TObject; AFields: TFields);
+var
+  F: TField;
+  I: Integer;
+  PI: PPropInfo;
+begin
+  if not Assigned(APropList) then
+    raise EdException.Create('APropList must not be nil.');
+  for I := 0 to Pred(APropCount) do
+  begin
+    PI := APropList^[I];
+    F := AFields.FindField(PI^.Name);
+    if not Assigned(F) then
+      Continue;
+    case PI^.PropType^.Kind of
+      tkAString: F.AsString := GetStrProp(AObject, PI);
+      tkChar: PChar(F.AsString)^ := Char(GetOrdProp(AObject, PI));
+      tkInteger: F.AsInteger := GetOrdProp(AObject, PI);
+      tkInt64, tkQWord: F.AsLargeInt := GetInt64Prop(AObject, PI);
+      tkBool: F.AsBoolean := GetOrdProp(AObject, PI) <> 0;
+      tkFloat: F.AsFloat := GetFloatProp(AObject, PI);
+      tkEnumeration: F.AsString := GetEnumProp(AObject, PI);
+      tkSet: F.AsString := GetSetProp(AObject, PI, False);
+    end;
+  end;
+end;
+
+procedure dSetFields(AObject: TObject; AFields: TFields);
+var
+  C: Integer;
+  PL: PPropList = nil;
+begin
+  C := GetPropList(PTypeInfo(AObject.ClassInfo), PL);
+  if Assigned(PL) then
+    try
+      dUtils.dSetFields(PL, C, AObject, AFields);
+    finally
+      FreeMem(PL);
+    end;
+end;
+
 procedure dGetParams(AObject: TObject; AParams: TParams);
 var
   I: Integer;
@@ -97,65 +143,43 @@ begin
   end;
 end;
 
-procedure dSetFields(AObject: TObject; AFields: TFields);
+procedure dSetParams(APropList: PPropList; const APropCount: Integer;
+  AObject: TObject; AParams: TParams);
 var
-  C, I: Integer;
-  F: TField;
+  P: TParam;
+  I: Integer;
   PI: PPropInfo;
-  PL: PPropList = nil;
 begin
-  C := GetPropList(PTypeInfo(AObject.ClassInfo), PL);
-  if Assigned(PL) then
-    try
-      for I := 0 to Pred(C) do
-      begin
-        PI := PL^[I];
-        F := AFields.FindField(PI^.Name);
-        if not Assigned(F) then
-          Continue;
-        case PI^.PropType^.Kind of
-          tkAString: F.AsString := GetStrProp(AObject, PI);
-          tkChar: PChar(F.AsString)^ := Char(GetOrdProp(AObject, PI));
-          tkInteger: F.AsInteger := GetOrdProp(AObject, PI);
-          tkInt64, tkQWord: F.AsLargeInt := GetInt64Prop(AObject, PI);
-          tkBool: F.AsBoolean := GetOrdProp(AObject, PI) <> 0;
-          tkFloat: F.AsFloat := GetFloatProp(AObject, PI);
-          tkEnumeration: F.AsString := GetEnumProp(AObject, PI);
-          tkSet: F.AsString := GetSetProp(AObject, PI, False);
-        end;
-      end;
-    finally
-      FreeMem(PL);
+  if not Assigned(APropList) then
+    raise EdException.Create('APropList must not be nil.');
+  for I := 0 to Pred(APropCount) do
+  begin
+    PI := APropList^[I];
+    P := AParams.FindParam(PI^.Name);
+    if not Assigned(P) then
+      Continue;
+    case PI^.PropType^.Kind of
+      tkAString: P.AsString := GetStrProp(AObject, PI);
+      tkChar: PChar(P.AsString)^ := Char(GetOrdProp(AObject, PI));
+      tkInteger: P.AsInteger := GetOrdProp(AObject, PI);
+      tkInt64, tkQWord: P.AsLargeInt := GetInt64Prop(AObject, PI);
+      tkBool: P.AsBoolean := GetOrdProp(AObject, PI) <> 0;
+      tkFloat: P.AsFloat := GetFloatProp(AObject, PI);
+      tkEnumeration: P.AsString := GetEnumProp(AObject, PI);
+      tkSet: P.AsString := GetSetProp(AObject, PI, False);
     end;
+  end;
 end;
 
 procedure dSetParams(AObject: TObject; AParams: TParams);
 var
-  C, I: Integer;
-  P: TParam;
-  PI: PPropInfo;
+  C: Integer;
   PL: PPropList = nil;
 begin
   C := GetPropList(PTypeInfo(AObject.ClassInfo), PL);
   if Assigned(PL) then
     try
-      for I := 0 to Pred(C) do
-      begin
-        PI := PL^[I];
-        P := AParams.FindParam(PI^.Name);
-        if not Assigned(P) then
-          Continue;
-        case PI^.PropType^.Kind of
-          tkAString: P.AsString := GetStrProp(AObject, PI);
-          tkChar: PChar(P.AsString)^ := Char(GetOrdProp(AObject, PI));
-          tkInteger: P.AsInteger := GetOrdProp(AObject, PI);
-          tkInt64, tkQWord: P.AsLargeInt := GetInt64Prop(AObject, PI);
-          tkBool: P.AsBoolean := GetOrdProp(AObject, PI) <> 0;
-          tkFloat: P.AsFloat := GetFloatProp(AObject, PI);
-          tkEnumeration: P.AsString := GetEnumProp(AObject, PI);
-          tkSet: P.AsString := GetSetProp(AObject, PI, False);
-        end;
-      end;
+      dUtils.dSetParams(PL, C, AObject, AParams);
     finally
       FreeMem(PL);
     end;
